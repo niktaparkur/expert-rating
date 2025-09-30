07006cc1f52eb10ba5af084ddeebc60a8bd75b04
niktaparkur <153425457+niktaparkur@users.noreply.github.com>
2025-09-30 22:24:52 +0300

    feat: setup database models, alembic and CI pipeline

--- alembic/env.py
+++ alembic/env.py
@@ -1,33 +1,26 @@
+import os
 from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
-config = context.config
+from dotenv import load_dotenv
+load_dotenv()
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
-
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-# target_metadata = None
-
-from app.models.base import Base
+from app.models.all_models import Base
 target_metadata = Base.metadata
 
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+config = context.config
 
+db_url = os.environ.get("ALEMBIC_DATABASE_URL")
+if not db_url:
+    raise ValueError("Необходимо установить переменную окружения DATABASE_URL")
+
+config.set_main_option("sqlalchemy.url", db_url)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
